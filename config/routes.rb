4b5b9e3d1a97dafb90c1f0f1a6b3bf5afa2f9836
Future <futureworldzone@gmail.com>
2023-10-18 18:26:12 +0300

    Updated routes for the rest of the database models

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,22 @@ Rails.application.routes.draw do
   resources :admin, only: [:index, :create, :update, :destroy]
   resources :client, only: [:index, :create, :update, :destroy]
   resources :trainer, only: [:index, :create, :update, :destroy]
+
+  # Client/Member routes
+  resources :client_membership, only: [:index, :create, :update, :destroy]
+  resources :client_payment, only: [:index, :create]
+  resources :gym_review, only: [:index, :create, :update, :destroy]
+  resources :progress_tracking, only: [:index, :create, :update, :destroy]
+
+  # Trainer/Staff routes
+  resources :client_review, only: [:index, :create, :update, :destroy]
+  resources :client_trainer_relationship, only: [:index, :create, :update, :destroy]
+
+  # Administrator routes
+  resources :role, only: [:index, :create, :update, :destroy]
+  resources :permissions, only: [:index, :create, :update, :destroy]
+  resources :trainer_performance, only: [:index, :create, :update, :destroy]
+  resources :gym_equipment, only: [:index, :create, :update, :destroy]
+  resources :gym_maintenance, only: [:index, :create, :update, :destroy]
+  
 end
