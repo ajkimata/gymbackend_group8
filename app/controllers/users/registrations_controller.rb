e6e1f4218b81827515969cdb38b99e74bd7a6647
alfred kimata <alfyjason@gmail.com>
2023-10-19 10:33:47 +0300

    user authentication

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,62 +1,39 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  # Uncomment the following lines if you have specific configurations for sign up or account update
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  respond_to :json
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  private
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  # Responds with the appropriate JSON based on the resource's state
+  def respond_with_resource(resource)
+    if resource.persisted?
+      render_successful_response(resource)
+    else
+      render_error_response(resource)
+    end
+  end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+  def render_successful_response(resource)
+    render json: {
+      status: {
+        code: 200,
+        message: 'Signed up successfully',
+        data: resource
+      }
+    }, status: :ok
+  end
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def render_error_response(resource)
+    render json: {
+      status: {
+        message: 'User could not be created successfully',
+        errors: resource.errors.full_messages
+      }
+    }, status: :unprocessable_entity
+  end
 end
