9797785fb0f1c153fa6f0c724bbb6309fe3b6aa5
alfred kimata <alfyjason@gmail.com>
2023-10-24 20:34:06 +0300

    cred file

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,91 +3,74 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
-  # # POST /users/sign_in
-  # def create
-  #   email = params.dig(:user, :email)&.downcase
-  #   password = params.dig(:user, :password)
+  # POST /users/sign_in
+  def create
+    user_params = params.require(:user).permit(:email, :username, :password)
 
-  #   # Logging sanitized params for debugging
-  #   logger.debug "Params received: #{params.except(:password, :password_confirmation).inspect}"
+    identifier = user_params[:email].presence || user_params[:username]
+    password = user_params[:password]
 
-  #   if email && password
-  #     authenticate_and_respond(email, password)
-  #   else
-  #     logger.warn 'Missing email or password in request'
-  #     render_unauthorized_response
-  #   end
-  # end
+    # Logging sanitized params for debugging
+    logger.debug "Processed Params: #{user_params.except(:password).inspect}"
 
-  # private
+    if identifier && password
+      authenticate_and_respond(identifier, password)
+    else
+      logger.warn 'Missing identifier or password in request'
+      render_unauthorized_response
+    end
+  end
 
-  # def authenticate_and_respond(email, password)
-  #   logger.info "Attempting to sign in user with email: #{email}"
-
-  #   user = User.find_by(email: email)
-  #   if user&.valid_password?(password)
-  #     logger.info "User with email: #{email} authenticated successfully"
-  #     jwt_token = generate_jwt_token_for_user(user)
-  #     response.headers['Authorization'] = "Bearer #{jwt_token}"
-  #     render json: {
-  #       status: {
-  #         code: 200,
-  #         message: 'User signed in successfully',
-  #         data: {
-  #           token: jwt_token,
-  #           user: user
-  #         }
-  #       }
-  #     }, status: :ok
-  #   else
-  #     logger.warn "Failed to authenticate user with email: #{email}"
-  #     render_unauthorized_response
-  #   end
-  # end
-
-  # def generate_jwt_token_for_user(user)
-  #   jti = SecureRandom.uuid
-  #   user.update(jti: jti) # Store the jti in the user record
-
-  #   payload = {
-  #     user_id: user.id,
-  #     jti: jti
-  #   }
-
-  #   JWT.encode(payload, Rails.application.credentials.fetch(:secret_key_base))
-  # rescue StandardError => e
-  #   logger.error "Error generating JWT token: #{e.message}"
-  #   raise e
-  # end
-
-  # def render_unauthorized_response
-  #   logger.warn 'Rendered unauthorized response'
-  #   render json: {
-  #     status: {
-  #       code: 401,
-  #       message: 'Unauthorized'
-  #     }
-  #   }, status: :unauthorized
-  # end
-
-  # def jwt_token
-  #   token = request.headers['Authorization']&.split(' ')&.last
-  #   logger.debug "Extracted JWT token from request header: #{token}"
-  #   token
-  # end
-
-  # def jwt_payload
-  #   payload = JWT.decode(
-  #     jwt_token,
-  #     Rails.application.credentials.fetch(:secret_key_base)
-  #   ).first
-  #   logger.debug "Decoded JWT payload: #{payload.inspect}"
-  #   payload
-  # rescue JWT::DecodeError => e
-  #   logger.error "Error decoding JWT token: #{e.message}"
-  #   nil
-  # end
   private
+
+  def authenticate_and_respond(identifier, password)
+    # Attempt to find the user by email or username
+    user = User.find_by(email: identifier) || User.find_by(username: identifier)
+
+    if user&.valid_password?(password)
+      logger.info "User with identifier: #{identifier} authenticated successfully"
+      jwt_token = generate_jwt_token_for_user(user)
+      response.headers['Authorization'] = "Bearer #{jwt_token}"
+      render json: {
+        status: {
+          code: 200,
+          message: 'User signed in successfully',
+          data: {
+            token: jwt_token,
+            user: user
+          }
+        }
+      }, status: :ok
+    else
+      logger.warn "Failed to authenticate user with identifier: #{identifier}"
+      render_unauthorized_response
+    end
+  end
+
+  def generate_jwt_token_for_user(user)
+    jti = SecureRandom.uuid
+    user.update(jti: jti) # Store the jti in the user record
+
+    payload = {
+      user_id: user.id,
+      jti: jti
+    }
+
+    JWT.encode(payload, Rails.application.credentials.fetch(:secret_key_base))
+  rescue StandardError => e
+    logger.error "Error generating JWT token: #{e.message}"
+    raise e
+  end
+
+  def render_unauthorized_response
+    logger.warn 'Rendered unauthorized response'
+    render json: {
+      status: {
+        code: 401,
+        message: 'Unauthorized'
+      }
+    }, status: :unauthorized
+  end
 
   # Overriding Devise's method to provide a custom response upon successful sign in
   def respond_with(_resource, _options = {})
